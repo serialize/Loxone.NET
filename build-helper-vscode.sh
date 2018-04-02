fb17cfa235b53bb9b0483de1c8a352b57fb95cd3
Frank <github@serialize.eu>
2018-04-02 06:54:24 +0200

    issue reference ncurses term added

--- build-helper-vscode.sh
+++ build-helper-vscode.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# is needed as workaround for a issue with ncurses and wrong default terminal
+# ref: https://github.com/dotnet/corefx/issues/26966#issuecomment-367066252
+  
 export TERM=xterm
 dotnet build $@
