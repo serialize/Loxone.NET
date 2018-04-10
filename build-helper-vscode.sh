d6ceaf8b7d66158eb3daafd93dd4464ef26e06b3
Frank <github@serialize.eu>
2018-04-10 23:58:37 +0200

    moved sourcefiles to src subdir

--- build-helper-vscode.sh
+++ build-helper-vscode.sh
@@ -2,6 +2,7 @@
 
 # is needed as workaround for a issue with ncurses and wrong default terminal
 # ref: https://github.com/dotnet/corefx/issues/26966#issuecomment-367066252
-  
+
+cd src
 export TERM=xterm
 dotnet build $@
